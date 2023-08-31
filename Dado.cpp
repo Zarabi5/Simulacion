f81a843bc327192e7298d7b82418dcafe4b81d42
zarina Hernandez <123223380+Zarabi5@users.noreply.github.com>
2023-08-31 01:14:25 -0500

    Add files via upload

--- Dado.cpp
+++ Dado.cpp
@@ -1,56 +1,80 @@
-solucion del codigo: # include <iostream>
-# include <math.h>
-# include <conio.h>
-# include <stdlib.h>
-# include <ctype.h>
-# include <iomanip>
-# include <string.h>
-# include <windows.h>
-# include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
+
+void Titulos(string titulo)
+{
+    cout << titulo << endl;
+}
+
+void Inicio3()
+{
+    system("cls");
+}
 
 int main(void)
 {
     int d1, d2;
-    int i:
-    system ("cls");  Titulos ("Dados.cpp");  Inicio3 ( ) ; 
+    int i;
+    
+    system("cls");
+    Titulos("Dados.cpp");
+    Inicio3();
+    
     srand(time(NULL));
-    cout << "Los dados numeros debe ser iguales " << endl;
-    cout << "#Tirada d1  d2  " endl;
+    
+    cout << "Los dados deben tener números iguales" << endl;
+    cout << "# Tirada  d1  d2" << endl;
     
     i = 1;
-    while ( 5 ==5 )
+    while (true)
     {
-          d1 = 1 + rand ( ) % 6;
-          d2 = 1 + rand ( ) % 6;
-          cout << setw(4) << i << setw(8) << d1
-               << setw(4) << d2 << endl;
-               if ( d1 == d2)
-               { break; }
-               i++;
+        d1 = 1 + rand() % 6;
+        d2 = 1 + rand() % 6;
+        
+        cout << setw(4) << i << setw(8) << d1 << setw(4) << d2 << endl;
+        
+        if (d1 == d2)
+        {
+            break;
+        }
+        
+        i++;
     }
-    cout  << "En " << i << "tiradas, son iguales"
-          << endl; << endl;
-    cout << "Oprima una tecla ..." << endl;     getch();
     
+    cout << "En " << i << " tiradas, los números son iguales" << endl << endl;
     
-    cout << "\n#Tirada  d1  d2"  << endl; 
-    i  = 1;
-    while ( 1 == 1 )
-    {    d1 = 1 + rand ( ) % 6;
-          d2 = 1 + rand ( ) % 6;
-          cout << setw(4) << i << setw(8) << d1
-               << setw(4) << d2 << endl;
-               if ( d1 == 6  && d2 == 6)
-               { break; }
-               i++;
-    } 
-    cout << "En " << i << "tiradas, son iguales a 6 "
-    << endl << endl; 
+    cout << "Oprima una tecla ..." << endl;
+    getch();
+    
+    cout << "\n# Tirada  d1  d2" << endl;
+    
+    i = 1;
+    while (true)
+    {
+        d1 = 1 + rand() % 6;
+        d2 = 1 + rand() % 6;
+        
+        cout << setw(4) << i << setw(8) << d1 << setw(4) << d2 << endl;
+        
+        if (d1 == 6 && d2 == 6)
+        {
+            break;
+        }
+        
+        i++;
+    }
+    
+    cout << "En " << i << " tiradas, ambos números son iguales a 6" << endl << endl;
     
     cout << "Proceso terminado. Oprima una tecla ....";
     getch();
-    return (0);
     
+    return 0;
 }
+
